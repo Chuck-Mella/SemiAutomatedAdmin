0faed53d4d5f9e36d5c7dc2d5fb867cd88001844
Carlos the Chuck <57195741+Chuck-Mella@users.noreply.github.com>
2025-02-13 08:17:37 -0700

    Create CM.PS_Assertions_profile.psm1

---
+++ Monsters From the PID/Profile Tweaks/ProfileAdds/CM.PS_Assertions_profile.psm1
@@ -0,0 +1,11 @@
+﻿# Folder Existence
+    filter Assert-FolderExists
+    {
+        $exists = Test-Path -Path $_ -PathType Container
+        if (!$exists)
+        { 
+            Write-Warning "$_ did not exist. Folder created."
+            $null = New-Item -Path $_ -ItemType Directory 
+        }
+        #  https://powershell.one/code/10.html
+    }  
