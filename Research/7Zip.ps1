dda1a16d676554ab1e0a65fce0c7d8a7cb4a7e33
Carlos the Chuck <57195741+Chuck-Mella@users.noreply.github.com>
2026-02-24 14:28:15 -0700

    Update base path for 7Zip script

--- Research/7Zip.ps1
+++ Research/7Zip.ps1
@@ -1,4 +1,4 @@
-$basPath = "C:\Users\CAMELLA\OneDrive - Federal Bureau of Investigation\Documents\Desktop"
+$basPath = "C:\Users\CAMELLA\OneDrive - Work\Documents\Desktop"
 $zipTool = "$basPath\7z2600-extra\7za.exe"
 $src     = "$basPath\Copilots"
 $dest    = "$basPath\Copilots.7z"
